b43674cae4555c370c4e8e9ecaf2777835c8458f
ch0fas <sof.mal.gar55@gmail.com>
2025-12-04 13:23:58 -0600

    2025 AOC - Day 2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # chofas-aoc
 My entries for the 2025 Advent of Code competition
+
+All were answered using Python 3.13.9 with no AI assistants / tools.
---
+++ d2_p1_gift_shop.py
@@ -0,0 +1,21 @@
+# Advent of Code - Day 2, Part 1
+# Link: https://adventofcode.com/2025/day/2
+
+example_nums = '11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124'
+puzzle_input = '78847-119454,636-933,7143759788-7143793713,9960235-10043487,44480-68595,23468-43311,89-123,785189-1014654,3829443354-3829647366,647009-692765,2-20,30-42,120909-197026,5477469-5677783,9191900808-9191943802,1045643-1169377,46347154-46441299,2349460-2379599,719196-779497,483556-641804,265244-450847,210541-230207,195-275,75702340-75883143,58-84,2152-3237,3367-5895,1552-2029,9575-13844,6048-8966,419388311-419470147,936-1409,9292901468-9292987321'
+
+invalid_ids = 0
+
+# ranges_example = [list(map(int, i.split('-'))) for i in example_nums.split(',')]
+# print(ranges_example)
+
+ranges_input = [list(map(int, i.split('-'))) for i in puzzle_input.split(',')]
+
+for i in ranges_input:
+    for j in range(i[0], i[1]+1):
+        stringed_num = str(j)
+        first_part, second_part = stringed_num[:len(stringed_num)//2], stringed_num[len(stringed_num)//2:]
+        if first_part == second_part:
+            invalid_ids += j
+
+print(f'All invalid ids: {invalid_ids}') # FINAL ANSWER -> 17077011375
